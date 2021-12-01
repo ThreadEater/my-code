1e7b957057b8d922864bd18a25641e3d9cd83098
ThreadEater <87987840+ThreadEater@users.noreply.github.com>
2021-12-01 11:49:34 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,3 @@
-import math
 def main():  
     j = 1
     factor = []
@@ -6,23 +5,11 @@ def main():
     number1 = number
     while(number != 1):
         j+=1
-        judge = True
-        for k in range(2,math.ceil(math.sqrt(j))):
-            if (j % k == 0):
-                judge == False
-        if (judge == True and number % j == 0):
+        if (number % j == 0):
             number /= j
-            factor.append(j)
+            factor.append(str(j))
             j = 1
-    print(number1,'=',end="")
-    for i in range(0,len(factor)):
-        if (i != len(factor)-1):
-            print(factor[i],end="*")
-        else:
-            print(factor[i])
+    print(number1,'=','*'.join(factor))
 if (__name__ == '__main__'):
-    main()
-                
-                
-                
+    main()      
             
