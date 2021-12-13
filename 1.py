f6ee80437447ce7d17481aa5bbdd3560b472cd5b
ThreadEater <87987840+ThreadEater@users.noreply.github.com>
2021-12-13 21:31:25 +0800

    第三次上机

--- 1.py
+++ 1.py
@@ -1,12 +1,30 @@
-dayPerMonth = [0,31,59,90,120,151,181,212,243,273,304,334,365]
-def main():
-    year,month,day = map(int,input().split())
-    if (month <= 2):
-        print(dayPerMonth[month - 1] + day)
-    elif (year % 4 == 0 and year % 400 != 0 and month > 2):
-        print(dayPerMonth[month - 1] + day + 1)
-    elif (year % 4 != 0 and month < 2 ):
-        print(dayPerMonth[month - 1] + day)
-    
-if __name__=="__main__":
-    main()
+from openpyxl import Workbook
+import pandas as pd
+
+df = pd.read_csv('score.txt',header = None)
+fn = 'score.xlsx'
+wb = Workbook()
+ws = wb.create_sheet(title='score')
+
+py1 =sorted(list((df[1])))
+py1.pop(0)
+py1.pop(-1)
+
+py2 =sorted(list((df[2])))
+py2.pop(0)
+py2.pop(-1)
+
+py3 =sorted(list((df[3])))
+py3.pop(0)
+py3.pop(-1)
+
+sc ={1:sum(py1)/8,2:sum(py2)/8,3:sum(py3)/8}
+rs = max(sc.items(),key =lambda x:x[1])
+
+ws['A1'] = rs[0]
+ws['B1'] = rs[1]
+ 
+wb.save(fn)
+
+
+
